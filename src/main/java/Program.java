ddd3b4b0875c5b720fb25a4d3cc71ec330af1a34
Ira <ira350520@gmail.com>
2024-11-09 13:02:07 +0300

    solution homework

---
+++ src/main/java/Program.java
@@ -0,0 +1,8 @@
+import app.Calculator;
+
+public class Program {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(5, 10));
+    }
+}
